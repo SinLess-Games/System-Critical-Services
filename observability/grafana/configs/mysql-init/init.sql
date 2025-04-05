8e5b611846cf67bb79bbdc03a8b8fda7ccfcf5d9
Sinless777 <disdainful777@gmail.com>
2025-04-05 21:29:46 +0000

    Add cloud-init metadata and user data for PXE boot; configure Grafana and Prometheus services; set up Nginx load balancer with logging; enhance Docker Compose files for observability services

--- observability/grafana/configs/mysql-init/init.sql
+++ observability/grafana/configs/mysql-init/init.sql
@@ -11,11 +11,16 @@ CREATE DATABASE IF NOT EXISTS grafana;
 CREATE USER 'grafana'@'%' IDENTIFIED BY 'grafana_password';
 CREATE USER 'dashboard'@'%' IDENTIFIED BY 'dashboard_password';
 CREATE USER 'oncall'@'%' IDENTIFIED BY 'oncall_password';
+CREATE USER 'exporter'@'%' IDENTIFIED BY 'exporter_password';
+CREATE USER 'sinless777'@'%' IDENTIFIED BY 'shellshocker93';
+
 
 /**
   * Give grafana all perms on grafana
   */
 GRANT ALL PRIVILEGES ON grafana.* TO 'grafana'@'%';
+GRANT PROCESS, REPLICATION CLIENT, SELECT ON *.* TO 'exporter'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'sinless777'@'%';
 
 /**
   * Give oncall all perms on oncall_hobby
